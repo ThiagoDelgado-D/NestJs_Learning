3256b6b6a9f6f79c704faae631efca2f09914cb8
Thiago <thiagodelgado.dev@gmail.com>
2023-12-21 11:17:05 -0300

    add environment variables

--- crud-cats-typeorm-docker/src/app.module.ts
+++ crud-cats-typeorm-docker/src/app.module.ts
@@ -8,21 +8,23 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     CatsModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_crud',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       autoLoadEntities: true,
       synchronize: false,
     }),
-    ConfigModule.forRoot(),
     BreedsModule,
     UsersModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [],
   providers: [],
